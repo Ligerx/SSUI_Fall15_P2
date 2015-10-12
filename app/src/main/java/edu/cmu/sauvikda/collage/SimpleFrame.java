6badc2ebeb933cc0f41cad9649ea6620a0afa2c6
awang1 <awang1@andrew.cmu.edu>
2015-10-12 16:14:58 -0400

    Make a simple frame class, but it is empty right now

---
+++ app/src/main/java/edu/cmu/sauvikda/collage/SimpleFrame.java
@@ -0,0 +1,10 @@
+package edu.cmu.sauvikda.collage;
+
+/**
+ * Created by admin on 10/12/15.
+ */
+public class SimpleFrame extends ArtistBase {
+    public SimpleFrame(float x, float y, float w, float h) {
+        // TODO
+    }
+}
