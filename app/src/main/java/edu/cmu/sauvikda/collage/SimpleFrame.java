2aaa948b38ecb623e9e08176c1ddd41e95fee761
awang1 <awang1@andrew.cmu.edu>
2015-10-12 18:44:53 -0400

    Simple constructor and draw method for simpleframe

--- app/src/main/java/edu/cmu/sauvikda/collage/SimpleFrame.java
+++ app/src/main/java/edu/cmu/sauvikda/collage/SimpleFrame.java
@@ -1,10 +1,27 @@
 package edu.cmu.sauvikda.collage;
 
-/**
- * Created by admin on 10/12/15.
- */
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
 public class SimpleFrame extends ArtistBase {
     public SimpleFrame(float x, float y, float w, float h) {
         // TODO
+        super();
+
+        setPosition(x, y);
+        setSize(w, h);
+    }
+
+    @Override
+    public void draw(Canvas onCanvas) {
+        Paint border = new Paint();
+        border.setColor(Color.BLACK);
+        border.setStyle(Paint.Style.STROKE);
+        border.setStrokeWidth(1);
+
+        onCanvas.drawRect(getX(), getY(), getW(), getH(), border);
+
+        super.draw(onCanvas);
     }
 }
